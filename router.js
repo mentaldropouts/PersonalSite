001da7ce2ab89ec499962ff848db9913c4c90a50
Taylor.Payne <taylor.payne@uky.edu>
2023-02-21 20:30:07 -0500

    inital Commit!

---
+++ router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = new express.Router();
+
+
+// router.route('/')   
+// .get((req,res)=>{
+// console.log("Home was accessed");
+// res.render('pagename');
+// })
+// .post((req,res)=>{
+// })
+
+module.exports = router;
